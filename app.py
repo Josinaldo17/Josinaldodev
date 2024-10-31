96aab7c74c9d0bfcc5c13aed0918601d87ea328f
JosinaldoRodrigues <josinaldor869@gmail.com>
2024-10-31 01:07:18 -0300

    Add portfolio

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/sobremim')
+def sobremim():
+    return render_template('sobreMim.html')
+
+@app.route('/projetos')
+def projetos():
+    return render_template('trabalhos.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
