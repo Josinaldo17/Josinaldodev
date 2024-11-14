eaac26e6b61401891243bb6804a29ba0dcf9bf15
JosinaldoRodrigues <josinaldor869@gmail.com>
2024-11-14 02:56:33 -0300

    Update

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def teste():
+    return '<h1>olaaaa</h1>'
+    
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+@app.route('/sobremim')
+def sobremim():
+    return render_template('sobreMim.html')
+
+@app.route('/projetos')
+def projetos():
+    return render_template('trabalhos.html')
