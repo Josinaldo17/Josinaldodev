c810d348e6c892ffa0f08f93abe1e1ce6759c025
JosinaldoRodrigues <josinaldor869@gmail.com>
2024-11-14 03:09:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def teste():
-    return '<h1>olaaaa</h1>'
+    return redirect('/home')
     
 @app.route('/home')
 def index():
