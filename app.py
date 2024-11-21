6f86f0f922844c3658f3b035b7237adf8651eefb
Josinaldo17 <146898937+Josinaldo17@users.noreply.github.com>
2024-11-21 00:40:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ def sobremim():
 def projetos():
     return render_template('trabalhos.html')
 
+@app.before_request
+def force_https():
+    if request.headers.get('X-Forwarded-Proto') == 'http':
+        return redirect(request.url.replace('http://', 'https://'), code=301)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
